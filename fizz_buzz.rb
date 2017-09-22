e5885e716f8d9ef5c402241c62bb68d7ce484e13
tamaraz <tamara.zlender@gmail.com>
2017-09-22 12:20:32 -0400

    My first program fizz buzz

---
+++ fizz_buzz.rb
@@ -0,0 +1,15 @@
+# # "Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number
+# and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”."
+(1..100).each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts num
+  end
+end
